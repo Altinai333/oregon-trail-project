59fe76bfdb0faebb61c89b8d25e87f2e60339075
Altinai333 <atoucheteau@putnamscience.org>
2024-11-12 13:53:52 -0500

    fix code

--- game altinai.py
+++ game altinai.py
@@ -1,91 +1,95 @@
-class BookStore():
-	def __init__(self):
-		self.Bnum = 0
-		self._Books = {} 
-		self._UIn = "n"
-		self._Name = "n"
-		self._Author = "n"
-		self._Price = float(0)
-		self._discount = 0
-		self._cash = float(1000)
-		self.fileload = "n"
+import random
+from random import randint
+distance = 0
+energy = 50
+sleep = 50 
+totaltravel = 0
+print ("Welcome to the immigration journey\nYou are a Mexican immigrant that wants to live in Florida")
+print ("Your journey will be long and hard, therefore you will have to make choices")
 
-	def add(self):
-		self._Name = input("enter the name of the book you would like to add: \n").upper()
-		try:
-			str(self._Name)
-			self._Author = input("enter the Author of the book you have just added: \n").upper()
-			if self._Author:
-				self._Price = float(input("Enter the listing price of the book:  \n"))
-				if self._Price > 0 and self._Price <= self._cash:
-					self._Books[self._Name] = {'Author': self._Author, 'Price': self._Price}
-					self._cash = self._cash - (self._Price*.9)
-					print (self._Books)
-					print("the remaining store balance is:", "${:,.2f}".format(self._cash))
-		except:
-			print("Please enter a book title")
-			
-	def sell(self):
-		self._Name = input("enter the name of the book you would like to sell: \n").upper()
-		try: 
-			str(self._Name)
-			if self._Name in self._Books:
-				self._cash = self._cash + self._Books[self._Name]['Price'] 
-				self._Books.pop(self._Name)
-				print("the new store balance is:", "${:,.2f}".format(self._cash))
-		except:
-			print("Please enter a book title")
+while totaltravel <= 300: 
+  longtravel = randint(20,30)
+  travel = randint(10,20)
+  rest = randint(10,20)
+  
 
-	def reduceA(self):
-		for x in self._Books:
-			self._Books[x]['Price'] = (self._Books[x]['Price']) * .98
-			print("the new cost of", self._Books[x]['Author'] + "'s", x, "is:", "${:,.2f}".format(self._Books[x]['Price']))
+  list = ["Monterey", "Houston", "Baton rouge", "Tallahasse"]
+  location = list [distance]
+  
+  print ("\nA. Travel at a slow pace \nB. Travel at a fast pace \nC. Eat for energy \nD. Rest for a day \nE. Check your location \nF. Learn about Mexican immigration \nG. Check your status \nQ. Quit")
 
-	def increaseA(self):
-		for x in self._Books:
-			self._Books[x]['Price'] = (self._Books[x]['Price']) * 1.02
-			print("the new cost of", self._Books[x]['Author'] + "'s", x, "is:", "${:,.2f}".format(self._Books[x]['Price']))
+  game = input("\nMake a choice ")
+  
+  if game.upper() == "A":
+    energy -= 5
+    sleep -= 5
+    totaltravel += travel 
+    print("You traveled", travel, "miles")
+    print("You traveled", totaltravel, "miles in total")
+    
+  
+  elif game.upper() == 'F':
+    list = ["There were 10.9 million Mexican immigrants in the US in 2023", "More than half of Mexican immigrants in the United States live in California or Texas", "Mexicans immigrate to the US for better job and growth opportunity"]
+    f = list 
+    computer = random.choice(list)
+    print(computer)
 
-	def showbook(self):
-		for x, y in self._Books.items():
-			print("\nBook title: "+ x +"\nAuthor: "+ self._Books[x]['Author'])
-			print("Buying Price:", "${:,.2f}".format(self._Books[x]['Price']*.9))
-			print("Selling Price:", "${:,.2f}".format(self._Books[x]['Price']))
-			print("Revenue from sale:",  "${:,.2f}".format(self._Books[x]['Price']*.1))
-		print("The Stores balance is:", "${:,.2f}".format(self._cash))
+ 
+  elif game.upper() == 'B':
+    energy -= 10
+    sleep -= 5
+    totaltravel += longtravel
+    print("You traveled", longtravel, "miles")
+    print("You traveled", totaltravel, "miles in total")
+    
 
-	def printout(self):
-		self.Bnum = 0
-		self.fileload = input("enter the file name you would like to print the inventory to: \n")
-		Writeout = open(self.fileload, "w")
-		for x, y in self._Books.items():
-			self.Bnum = self.Bnum + 1
-			Writeout.write(f'/* {self.Bnum} */\n')
-			Writeout.write(x+"\n")
-			Writeout.write(self._Books[x]['Author']+"\n")
-			Writeout.write(str(self._Books[x]['Price'])+"\n")
-		Writeout.write("The Stores balance is: " + "${:,.2f}".format(self._cash)+"\n\n")
-		print("The inventory has been successfully saved to: " + self.fileload)
 
-	def loadin(self):
-		self.fileload = input("enter the name of the file you would like to load the inventory from: \n")
-		infile = open(self.fileload, "r")
-		endcheck = infile.read(3)
-		while endcheck != "the":
-			line = infile.readline()
-			line = infile.readline()
-			self._Name = line.strip()
-			line = infile.readline()
-			self._Author = line.strip()
-			line = infile.readline()
-			self._Price = line.strip()
-			self._Price = float(self._Price)
-			self._Books[self._Name] = {'Author': self._Author, 'Price': self._Price}
-			endcheck = infile.read(3)
-		self._cash = line.lstrip("Storebalnci: $")
-		infile.close
+  elif game.upper() == 'C':
+    energy += 20
+    print("Your energy is", energy)
+    
+  
+  elif game.upper() == 'D':
+    sleep += rest
+    print("You rested for a day")
+    print("Your energy is now at", sleep )
+    
 
-	def qui(self, foop):
-		self._foop = False
-		return self._foop
+ 
+            
+  elif game.upper() == 'E':
+   if totaltravel < 70:
+       distance = 0
+       print("You are in",(list[distance]))
+   elif totaltravel < 150 and totaltravel > 69 :
+       distance = 1
+       print ("You are in",(list[distance]))
+   elif totaltravel < 250 and totaltravel > 149:
+       distance = 2
+       print ("You are in",(list[distance]))
+   elif totaltravel < 300 and totaltravel > 299:
+       distance = 3
+       print ("You are in",(list[distance]))
+    
+  if energy < 1:
+    print("You are dead")
+    quit()
 
+  if sleep < 1:
+    print("You are dead")
+    quit()
+
+  if game == 'Q':
+    quit()
+    
+
+  if game.upper() == 'G':
+   print("Your status is:") 
+   print("Your energy is", energy)
+   print("Your sleep is", sleep)
+   print("You traveled", totaltravel,"miles")
+   print("You have", (300 - totaltravel) , "miles left")
+  if totaltravel > 299:
+        print ("Congratulations !")
+        print ("You are in Tallahasse, Florida")
+        print("You can now enjoy your new life in America")
